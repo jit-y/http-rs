5a961a2413443eb995c232a1304773f3007c161b
YUJI takahashi <jit.y.31@gmail.com>
2020-08-08 22:24:49 +0900

    listen tcp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::Write;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+
+        std::process::exit(1);
+    }
+}
+
+fn run() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
+
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+
+        print!("{:?}", stream);
+
+        let resp = "OK".as_bytes();
+
+        stream.write(&resp)?;
+    }
+
+    Ok(())
 }
