f544bb412ae1a4c9a2d313878b313a77d0d665dd
YUJI takahashi <jit.y.31@gmail.com>
2020-08-09 23:22:49 +0900

    Buffering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufWriter, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -13,13 +13,13 @@ fn run() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:3000")?;
 
     for stream in listener.incoming() {
-        let mut stream = stream?;
-
-        print!("{:?}", stream);
+        let mut stream = BufWriter::new(stream?);
 
         stream.write(&"HTTP/1.1 200 OK\n".as_bytes())?;
         stream.write(&"Content-Length: 0\n".as_bytes())?;
         stream.write(&"\n".as_bytes())?;
+
+        stream.flush()?;
     }
 
     Ok(())
