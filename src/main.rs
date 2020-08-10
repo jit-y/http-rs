d084b32f7a3b6d97771d8ece58d29d071f55eb9a
YUJI takahashi <jit.y.31@gmail.com>
2020-08-10 14:04:35 +0900

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+#[macro_use]
+extern crate log;
+
 use std::io::{BufWriter, Write};
 use std::net::TcpListener;
 
 fn main() {
+    env_logger::init();
+
     if let Err(e) = run() {
         eprintln!("{}", e);
 
@@ -10,16 +15,23 @@ fn main() {
 }
 
 fn run() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:3000")?;
+    let addr = "127.0.0.1:3000";
+    let listener = TcpListener::bind(addr)?;
+
+    info!("Start server: {}", addr);
 
     for stream in listener.incoming() {
         let mut stream = BufWriter::new(stream?);
+
+        info!("start");
 
         stream.write(&"HTTP/1.1 200 OK\n".as_bytes())?;
         stream.write(&"Content-Length: 0\n".as_bytes())?;
         stream.write(&"\n".as_bytes())?;
 
         stream.flush()?;
+
+        info!("end");
     }
 
     Ok(())
